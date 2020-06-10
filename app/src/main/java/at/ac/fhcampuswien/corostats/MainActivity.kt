6ca1418a8db9fff8462d8c135a52c5ad52c3ff4e
starvis <nichil.strasser@gmail.com>
2020-06-11 00:57:57 +0200

    Initial commit

---
+++ app/src/main/java/at/ac/fhcampuswien/corostats/MainActivity.kt
@@ -0,0 +1,38 @@
+package at.ac.fhcampuswien.corostats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity(), NavigationHost {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, StatsFragment())
+                .commit()
+        }
+    }
+
+    /**
+     * Navigate to the given fragment.
+     *
+     * @param fragment       Fragment to navigate to.
+     * @param addToBackstack Whether or not the current fragment should be added to the backstack.
+     */
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
+    }
+}
