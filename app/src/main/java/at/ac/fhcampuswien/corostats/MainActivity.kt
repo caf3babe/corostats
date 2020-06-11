ffbd55ca9397f9c01aa853e76bb092c4e0bbf516
starvis <nichil.strasser@gmail.com>
2020-06-11 11:02:06 +0200

    added stats fragment and news fragment, updated bottom navigation

--- app/src/main/java/at/ac/fhcampuswien/corostats/MainActivity.kt
+++ app/src/main/java/at/ac/fhcampuswien/corostats/MainActivity.kt
@@ -1,38 +1,15 @@
 package at.ac.fhcampuswien.corostats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import at.ac.fhcampuswien.corostats.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), NavigationHost {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.container, StatsFragment())
-                .commit()
-        }
-    }
-
-    /**
-     * Navigate to the given fragment.
-     *
-     * @param fragment       Fragment to navigate to.
-     * @param addToBackstack Whether or not the current fragment should be added to the backstack.
-     */
-    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
-        val transaction = supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, fragment)
-
-        if (addToBackstack) {
-            transaction.addToBackStack(null)
-        }
-
-        transaction.commit()
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
--- app/src/main/java/at/ac/fhcampuswien/corostats/NewsFragment.kt
+++ app/src/main/java/at/ac/fhcampuswien/corostats/NewsFragment.kt
@@ -1,18 +1,18 @@
 package at.ac.fhcampuswien.corostats
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 
-class StatsFragment : Fragment() {
+class NewsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stats, container, false)
+        return inflater.inflate(R.layout.fragment_news, container, false)
     }
 }
